94bc0bf9924fa4c87c09c60879e9d82483aa7f74
tletutour <tom.le-tutour@insa-lyon.fr>
2019-03-02 12:26:23 +0100

    Nouvelle génération de forme "clouds" pour commencer apprentissage de templates

--- TemplateMatching/generation_img.py
+++ TemplateMatching/generation_img.py
@@ -11,40 +11,45 @@ from skimage.draw import (line, polygon, circle,
                           ellipse, ellipse_perimeter,
                           bezier_curve)
 
-path = os.getcwd()+"/circles/"
+pathCircle = os.getcwd()+"/circles/"
+pathCloud= os.getcwd()+"/clouds/"
+pathTemplate= os.getcwd()+"/templates/"
 try:  
-    os.mkdir(path)
+    os.mkdir(pathCircle)
+    os.mkdir(pathCloud)
+    os.mkdir(pathTemplate)
 except OSError:  
-    print ("Creation of the directory %s failed" % path)
-
-fig, (ax2) = plt.subplots(ncols=1, nrows=1, figsize=(10, 6))
+    print ("Creation of a directory failed")
 
 
 corruption=0.2#for corrupting randomly, must be between 0.0 and 1.0
 i=0
-for i in range(100):
-    
+for i in range(100):    
     img = np.zeros((500, 500), dtype=np.int16)
-    '''
-    rr, cc, val = circle_perimeter_aa(20+random.randint(-5,5), 30+random.randint(-1,10), 30+random.randint(-1,15))
-    img[rr, cc] = val
-    '''
     # fill circle
     rr, cc = circle(random.randint(0,500), random.randint(0,500), 50+random.randint(-3,3), img.shape)
     img[rr, cc] =255
-    '''    
-    rr, cc, val = circle_perimeter_aa(50+random.randint(-15,15), 70+random.randint(-10,10),5+random.randint(-4,3))
-    img[rr, cc] = val
-     '''   
-    ax2.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
-    ax2.set_title('Created image')
-    ax2.axis('off')
-        
-    plt.show()
-    save(path+"/circles"+str(i)+".png", img)
+    save(pathCircle+"/circles"+str(i)+".jpg", img)
+
+
+for j in range(100):
+    img = np.zeros((500, 500), dtype=np.int16)
+    rr, cc = circle(100+random.randint(-10,10), 100+random.randint(-10,10), 50+random.randint(-3,3), img.shape)
+    img[rr, cc] =255
+    
+    rr, cc = circle(160+random.randint(-10,10), 100+random.randint(-10,10), 20+random.randint(-3,3), img.shape)
+    img[rr, cc] =255
+    
+    rr, cc = circle(190+random.randint(-10,10), 100+random.randint(-10,10), 30, img.shape)
+    img[rr, cc] =255
+    save(pathCloud+"clouds"+str(j)+".jpg", img)
+
 
 #declaring the template to slide (here it's a circle)
 tmplt = np.zeros((100, 100), dtype=np.int16)
 rr, cc = circle(50,50, 50, tmplt.shape)
 tmplt[rr, cc] =255
-save(path+"aTemplate.png", tmplt)
+save(pathTemplate+"aTemplate.png", tmplt)
+
+
+    
