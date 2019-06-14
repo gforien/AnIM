e26c21b2fb8227d2dc2cf4484cf5eda14aabb216
tletutour <tom.le-tutour@insa-lyon.fr>
2019-06-14 10:08:52 +0200

    ajout main.py + util pyramide working !

--- TemplateMatching/templateMaker.py
+++ TemplateMatching/templateMaker.py
@@ -11,10 +11,31 @@ fullpath="/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb"
 recep_path="/media/tom/TOSHIBA EXT"
 segfiles = os.listdir( segpath )#list of all files in working dir
 typicalfilename="10000100_1_CTce_ThAb_2_0.nii.gz"
-classtolabel={} #sidequest but who cares
+classtolabel={
+    1247:"trachea",
+    1302:"right lung",
+    1326:"left lung",
+    170:"pancreas",
+    187:"gallbladder",
+    237:"urinary bladder",
+    2473:"sternum",
+    29193:"first lumbar vertebra",
+    29662:"right kidney",
+    29663 :"left kidney",
+    30324:"right adrenal gland",
+    30325:"left adrenal gland",
+    32248:"right psoas major",
+    32249:"left psoas major",
+    40357:"muscle body of right rectus abdominis",
+    40358:"muscle body of left rectus abdominis",
+    480:"aorta",
+    58:"liver",
+    7578:"thyroid gland",
+    86:"spleen"
+}
 patients=["100","104","105","106","108","109","110","111","112","113","127","128","129","130","131","132","133","134","135","136"]
 #patients=["100","104","105","106","108"]
-classes=["58","1302","1247","1326","170","187" ,"237","2473" ,"29193" ,"29662" ,"29663" ,"30324" ,"30325" ,"32248" ,"32249" ,"40357" ,"40358" ,"480"  ,"7578" ,"86" ,"0" ,"1","2" ]
+classes=["58","1302","1247","1326","170","187" ,"237","2473" ,"29193" ,"29662" ,"29663" ,"30324" ,"30325" ,"32248" ,"32249" ,"40357" ,"40358" ,"480"  ,"7578" ,"86"]
 #classes=["1302","1247","1326","170"]
 #classes=["58"]
 
@@ -111,6 +132,60 @@ def make_templates(excluded_patient):
         #nib.save(sumofall,path_to_save )
         print("done with ",classe)
 
+def get_classes():
+    return classes
+
+
+def exact_specific():
+    full=nib.load("/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb/10000100_1_CTce_ThAb.nii.gz")
+    mask = nib.load("/media/tom/TOSHIBA EXT/visceral/segmentations/CTce_ThAb/10000100_1_CTce_ThAb_58_6.nii.gz")
+    mer = merge(mask, full)
+    mer = bbox2_3D(mer)
+    path_to_save = '/media/tom/TOSHIBA EXT/PIR/100/exact_58.nii.gz'
+    img = nib.Nifti1Image(mer, full.get_affine())
+    img.to_filename(path_to_save)
+
+def as_segmentation(excluded_patient):
+    try:
+        os.mkdir(os.path.join('/media/tom/TOSHIBA EXT/PIR',excluded_patient))
+    except Exception as e: print(e)
+
+    valid_dirs=[]
+    for directory in segfiles:
+        if get_patient(directory)!=excluded_patient and get_patient(directory) in patients:
+            valid_dirs.append(directory)
+    for classe in classes:
+        print("starting class",classe)
+        sumofall=np.zeros((512,512,413))
+        affine_sum=np.zeros((4,4))
+        nb=0
+        for directory in valid_dirs:
+            if(get_class(directory)==classe):
+                nb+=1
+                print("\t patient"+get_patient(directory))
+                vol_filename="10000"+get_patient(directory)+"_1_CTce_ThAb.nii.gz"
+                vol_dir=os.path.join(fullpath,vol_filename)
+                seg_dir=os.path.join(segpath,directory)
+                #load images with nibabels
+                mask=nib.load(seg_dir)
+                full=nib.load(vol_dir)
+                #merge, englob, and append to list
+                mer=merge(mask,full)
+                mer=bbox2_3D(mer)
+                affine_sum=affine_sum+mask.affine
+                sumofall=add_two(sumofall,mer)
+                #sumofall=bbox2_3D(sumofall)
+        sumofall=np.dot(1/nb,sumofall)
+        affine_sum = np.dot(1 / nb, affine_sum)
+        sumofall=gaussian_filter(sumofall, sigma=5)
+        path_to_save=os.path.join('/media/tom/TOSHIBA EXT/PIR',str(excluded_patient),str(classe)+'as_seg.nii.gz')
+        img = nib.Nifti1Image(sumofall, affine_sum)
+        img.to_filename(path_to_save)
+        #nib.save(sumofall,path_to_save )
+        print("done with ",classe)
 if __name__=="__main__":
     for patient in patients:
-        make_templates(patient)
+        #make_templates(patient)
+        #exact_specific()
+        pass
+    as_segmentation(100)
--- TemplateMatching/util.py
+++ TemplateMatching/util.py
@@ -4,14 +4,14 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import os
+from multiprocessing import Process, Array,Pool
 import nibabel as nib
+import random
 import matplotlib.patches as patches
 from scipy.signal import correlate as correlate
 from scipy.signal import fftconvolve as fftconvolve
 import time
 from skimage.transform import pyramid_gaussian,downscale_local_mean
-
-
 import gc
 from templateMaker import bbox2_3D,broadcast_2d,broadcast_3d,get_patient
 
@@ -21,8 +21,8 @@ def shotgun(image,template,factor):
     dim_factor=(factor ,factor,factor)
     print("image shape",image.shape)
     print("template shape",template.shape)
-    resized_image=downscale_local_mean(image,dim_factor)
-    resized_template=downscale_local_mean(template,dim_factor)
+    resized_image=downscale_local_mean(image,(factor,factor,factor))
+    resized_template=downscale_local_mean(template,(factor,factor,factor))
     print("resized image shape",resized_image.shape)
     print("resized template shape",resized_template.shape)
     nxcorr=fft_nxcorr(resized_image,resized_template)
@@ -30,14 +30,19 @@ def shotgun(image,template,factor):
     maxind=np.unravel_index(np.argmax(nxcorr, axis=None), nxcorr.shape)
     print("corr max is ",np.amax(nxcorr))
     print("at",maxind)
-
     relative_index=(maxind[0]/resized_image.shape[0],
                     maxind[1]/resized_image.shape[1],
                     maxind[2]/resized_image.shape[2])
+    display_ind = (int(relative_index[0] * resized_image.shape[0]),
+                    int(relative_index[1] * resized_image.shape[1]),
+                    int(relative_index[2] * resized_image.shape[2]))
+    display_square(resized_image,resized_template.shape,display_ind)
     abs_ind=(int(relative_index[0]*image.shape[0]),
              int(relative_index[1]*image.shape[1]),
              int(relative_index[2]*image.shape[2]))
-    sigma = 1
+    print("absolute index is ",abs_ind)
+
+    sigma = 5
     del (resized_image)
     del (resized_template)
     del (nxcorr)
@@ -58,15 +63,66 @@ def shotgun(image,template,factor):
     print("second bam !")
     print("corr max is ", np.amax(nxcorr))
     print("at", maxind)
-    display_square(image,template.shape,maxind)
     return maxind
 
-def pyr(pyramid,template,layer,ind=(0.0,0.0,0.0)):
-    print("couche ",layer)
+def display_square(image,tshape, maxind,solution=None):
+    x_t,y_t,z_t=tshape
+    fig, [ax1 , ax2 , ax3] = plt.subplots(1, 3, num='Result of Template Search')
+    rectxy = patches.Rectangle((maxind[1] - int(y_t / 2), maxind[0] - int(x_t / 2)), y_t, x_t, linewidth=1,
+                             edgecolor='r', facecolor='none')
+    rectyz = patches.Rectangle((maxind[2] - int(z_t / 2), maxind[1] - int(y_t / 2)), z_t, y_t, linewidth=1,
+                               edgecolor='r', facecolor='none')
+    rectxz = patches.Rectangle((maxind[2] - int(z_t / 2), maxind[0] - int(x_t / 2)), z_t, x_t, linewidth=1,
+                               edgecolor='r', facecolor='none')
+
+    ax1.imshow(image[:,:,maxind[2]], interpolation='nearest')
+    ax1.add_patch(rectxy)
+    ax2.imshow(image[maxind[0], :,:], interpolation='nearest')
+    ax2.add_patch(rectyz)
+    ax3.imshow(image[:, maxind[1], :], interpolation='nearest')
+    ax3.add_patch(rectxz)
+    if solution!=None:
+        x_s,y_s,z_s=solution
+        solxy = patches.Rectangle((maxind[1] - int(y_t / 2), x_s - int(x_t / 2)), y_t, x_t, linewidth=1,
+                                   edgecolor='g', facecolor='none')
+        solyz = patches.Rectangle((maxind[2] - int(z_t / 2), y_s - int(y_t / 2)), z_t, y_t, linewidth=1,
+                                   edgecolor='g', facecolor='none')
+        solxz = patches.Rectangle((maxind[2] - int(z_t / 2), z_s - int(x_t / 2)), z_t, x_t, linewidth=1,
+                                   edgecolor='g', facecolor='none')
+        ax1.add_patch(solxy)
+        ax2.add_patch(solyz)
+        ax3.add_patch(solxz)
+    plt.show()
+
+
+
+
+def pyr(pyramid,template,layer,max_recursion,ind=(0.0,0.0,0.0)):
+    #print("couche ",layer)
+    if layer<1:
+        #print("last layer")
+        image=pyramid[0]
+        oneachside=50
+        cx,cy,cz=ind
+        xinf,xsup=int(cx * image.shape[0] - oneachside),int(cx * image.shape[0] + oneachside),
+        yinf,ysup=int(cy * image.shape[1] - oneachside), int(cy * image.shape[1] + oneachside),
+        zinf,zsup=int(cz * image.shape[2] - oneachside), int(cz * image.shape[2] + oneachside)
+
+        last_region=image[xinf:xsup,yinf:ysup,zinf:zsup]
+        nxcorr = fft_nxcorr(f=last_region, t=template)
+        corrind = np.unravel_index(np.argmax(nxcorr, axis=None), nxcorr.shape)
+        realind = (xinf + corrind[0], yinf + corrind[1], zinf + corrind[2])
+        print("\trealind", realind)
+        #print("\tcorr index", corrind)
+        #print("\tnxcorr max", np.amax(nxcorr))
+        print("\tcorr max",np.amax(nxcorr))
+        return realind
+
     couche=pyramid[layer]
-    print("\tcouche shape",couche.shape)
-    print('relative ind passed to this layer is', ind )
+    #print("\tcouche shape",couche.shape)
+    #print('relative ind passed to this layer is', ind )
     max_layer=len(pyramid)-1
+
     #Ratio in every dimension between image and template dimensions
     #NOTE : this doesn't change during the whole procedure
     ratios=(template.shape[0]/pyramid[0].shape[0],
@@ -82,25 +138,25 @@ def pyr(pyramid,template,layer,ind=(0.0,0.0,0.0)):
     #Here's where we downscale
     r_t=downscale_local_mean(template,downscaling_factor)
     #display(couche, r_t)
-    print("\tresied template shape",r_t.shape)
-    print("\texected shape is",resized_shape)
+    #print("\tresied template shape",r_t.shape)
+    #print("\texected shape is",resized_shape)
     #We now have to calculate the borders region of interest
     #It is based on previous iterations (or not if it is the first one)
-    if(layer==max_layer-3):
+    if(layer==max_recursion):
         #the layer is the maximum one we need togo through the whole image
         nxcorr = fft_nxcorr(f=couche, t=r_t)
         corrind = np.unravel_index(np.argmax(nxcorr, axis=None), nxcorr.shape)
-        print("\t corr index",corrind)
+        #print("\t corr index",corrind)
         realind = (corrind[0], corrind[1], corrind[2])
     else:
         #We have to calculate what is the absolute index in our case
         abs_ind=(int(couche.shape[0]*ind[0]),
                  int(couche.shape[1]*ind[1]),
                  int(couche.shape[2]*ind[2]))
-        print("\tabs ind",abs_ind)
+        #print("\tabs ind",abs_ind)
         #We use a parameter sigma that will determine how big the region is
         #Let's establish that if sigma = 1 the RoI is of one template
-        sigma=1.1
+        sigma=1.5
         xinf,xsup=abs_ind[0]-int(sigma*resized_shape[0]/2),abs_ind[0]+int(sigma*resized_shape[0]/2)
         yinf, ysup = abs_ind[1] - int(sigma * resized_shape[1] / 2), abs_ind[1] + int(sigma * resized_shape[1] / 2)
         zinf, zsup = abs_ind[2] - int(sigma * resized_shape[2] / 2), abs_ind[2] + int(sigma * resized_shape[2] / 2)
@@ -108,29 +164,31 @@ def pyr(pyramid,template,layer,ind=(0.0,0.0,0.0)):
         if xinf<0:xinf=0
         if yinf<0:yinf=0
         if zinf<0:zinf=0
-        if xsup> couche.shape[0]:xsup=couche.shape[0]-1
+        if xsup > couche.shape[0]:xsup=couche.shape[0]-1
         if ysup > couche.shape[1]: xsup = couche.shape[1] - 1
         if zsup > couche.shape[2]: xsup = couche.shape[2] - 1
-        print("\t RoI shape ",couche[xinf:xsup,yinf:ysup,zinf:zsup].shape)
+        #print("\t RoI shape ",couche[xinf:xsup,yinf:ysup,zinf:zsup].shape)
         #We correlate with a view of the image rather than another object
         #to have the max in the coordinates of the image
         nxcorr=fft_nxcorr(f=couche[xinf:xsup,yinf:ysup,zinf:zsup],t=r_t)
         corrind = np.unravel_index(np.argmax(nxcorr, axis=None), nxcorr.shape)
         realind = (xinf + corrind[0], yinf + corrind[1], zinf + corrind[2])
-        print("corr index",corrind)
-        print("\tnxcorr max",np.amax(nxcorr))
+        #print("corr index",corrind)
+        #print("\tnxcorr max",np.amax(nxcorr))
         #display(nxcorr,r_t)
     # Find max in corr
     corrind = np.unravel_index(np.argmax(nxcorr, axis=None), nxcorr.shape)
-    print("max in corr at",corrind)
+    #print("max in corr at",corrind)
 
-    print("\trealind",realind)
+    #print("\trealind",realind)
     relative_ind=(realind[0] / couche.shape[0], realind[1] / couche.shape[1], realind[2] / couche.shape[2])
-    if layer<=1:
-        return ind
-    return pyr(pyramid, template, layer - 1, relative_ind)
 
-def fft_nxcorr(f,t):
+    return pyr(pyramid, template, layer - 1,max_recursion,relative_ind)
+
+
+
+
+def fft_nxcorr(f,t,array=None,i=None):
     """
         This method is heavily influenced (and in some occasions copied)  by the code of
 
@@ -193,57 +251,15 @@ def fft_nxcorr(f,t):
     # unstable to small variance in a or t, so set them to zero to reflect
     # the undefined 0/0 condition
     nxcorr = np.where(np.abs(nxcorr - 1.) > np.sqrt(np.finfo(nxcorr.dtype).eps), nxcorr, 0)
+    if array!=None:
+        array[i]=np.amax(nxcorr)
+    #time.sleep(random.randint(1,3))
     return nxcorr
 
 
 
-def display(image,template):
-    fig, [[ax1, ax2, ax3],[ax4,ax5,ax6]] = plt.subplots(2, 3, num='Result of Template Search')
-    ax1.imshow(image[:, :, int(image.shape[2]/2)], interpolation='nearest')
-    ax1.set_title('image XY')
-    ax2.imshow(image[int(image.shape[0]/2), :, :], interpolation='nearest')
-    ax2.set_title('image YZ')
-    ax3.imshow(image[:,int(image.shape[1]/2), :], interpolation='nearest')
-    ax3.set_title('image XZ')
-    ax4.imshow(template[:, :, int(template.shape[2] / 2)], interpolation='nearest')
-    ax4.set_title('template XY')
-    ax5.imshow(template[int(template.shape[0] / 2), :, :], interpolation='nearest')
-    ax5.set_title('template YZ')
-    ax6.imshow(template[:, int(template.shape[1] / 2), :], interpolation='nearest')
-    ax6.set_title('template XZ')
-    plt.show()
-
-def display_square(image,tshape, maxind,solution=None):
-    x_t,y_t,z_t=tshape
-    fig, [ax1 , ax2 , ax3] = plt.subplots(1, 3, num='Result of Template Search')
-    rectxy = patches.Rectangle((maxind[1] - int(y_t / 2), maxind[0] - int(x_t / 2)), y_t, x_t, linewidth=1,
-                             edgecolor='r', facecolor='none')
-    rectyz = patches.Rectangle((maxind[2] - int(z_t / 2), maxind[1] - int(y_t / 2)), z_t, y_t, linewidth=1,
-                               edgecolor='r', facecolor='none')
-    rectxz = patches.Rectangle((maxind[2] - int(z_t / 2), maxind[0] - int(x_t / 2)), z_t, x_t, linewidth=1,
-                               edgecolor='r', facecolor='none')
-
-    ax1.imshow(image[:,:,maxind[2]], interpolation='nearest')
-    ax1.add_patch(rectxy)
-    ax2.imshow(image[maxind[0], :,:], interpolation='nearest')
-    ax2.add_patch(rectyz)
-    ax3.imshow(image[:, maxind[1], :], interpolation='nearest')
-    ax3.add_patch(rectxz)
-    if solution!=None:
-        x_s,y_s,z_s=solution
-        solxy = patches.Rectangle((maxind[1] - int(y_t / 2), x_s - int(x_t / 2)), y_t, x_t, linewidth=1,
-                                   edgecolor='g', facecolor='none')
-        solyz = patches.Rectangle((maxind[2] - int(z_t / 2), y_s - int(y_t / 2)), z_t, y_t, linewidth=1,
-                                   edgecolor='g', facecolor='none')
-        solxz = patches.Rectangle((maxind[2] - int(z_t / 2), z_s - int(x_t / 2)), z_t, x_t, linewidth=1,
-                                   edgecolor='g', facecolor='none')
-        ax1.add_patch(solxy)
-        ax2.add_patch(solyz)
-        ax3.add_patch(solxz)
-    plt.show()
-
-def load_images(fullpath='/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb/10000100_1_CTce_ThAb.nii.gz'
-                ,templatepath='/media/tom/TOSHIBA EXT/PIR/100/58.nii.gz'):
+def load_images(fullpath='/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb/10000108_1_CTce_ThAb.nii.gz'
+                ,templatepath='/media/tom/TOSHIBA EXT/PIR/108/1302.nii.gz'):
     print("loading image", fullpath)
     image = nib.load(fullpath)
     print("loading template", templatepath)
@@ -251,65 +267,5 @@ def load_images(fullpath='/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb/1000
 
     return image,template
 
-def routine_3d():
-    print("3d routine starting")
-    image,template=load_images()
-    #display(image.get_fdata(),template.get_fdata())
 
-    image_shape=image.get_fdata().shape
-    pyramid = tuple(pyramid_gaussian(image.get_fdata(), downscale=2, multichannel=False))
-    max_layer = len(pyramid) - 1
-    #template_rotate= np.rot90(template.get_fdata(), k=1)
-    ind=pyr(pyramid=pyramid,template=template.get_fdata(),layer=max_layer-3)
-
-    #ind=shotgun(image.get_fdata(),template.get_fdata(),3)
-    print("index is",ind)
-    inflated_index=(int(image.shape[0]*ind[0]),int(image.shape[1]*ind[1]),int(image.shape[2]*ind[2]))
-    #display_square(image.get_fdata(),template.shape,inflated_index)
-
-def test_classe(classe,templates_path='/media/tom/TOSHIBA EXT/PIR',results_path='media/tom/TOSHIBA EXT/PIR_RESULTATS',fullpath='/media/tom/TOSHIBA EXT/visceral/volumes/CTce_ThAb'):
-    print("testing classe",classe)
-    classe_template_name=str(classe)+".nii.gz"
-    all_patients = os.listdir(templates_path)
-    with open(str(classe)+'.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["patient", "i", "j","k"])
-        for imagename in os.listdir(fullpath):
-
-            image,template=load_images(fullpath=os.path.join(fullpath,imagename),templatepath=os.path.join(templates_path,get_patient(imagename),classe_template_name))
-            pyramid = tuple(pyramid_gaussian(image.get_fdata(), downscale=2, multichannel=False))
-            max_layer = len(pyramid) - 1
-            ind = pyr(pyramid=pyramid, template=template.get_fdata(), layer=max_layer - 3)
-            inflated_index = (int(image.shape[0] * ind[0]), int(image.shape[1] * ind[1]), int(image.shape[2] * ind[2]))
-            writer.writerow([get_patient(imagename),inflated_index[0],inflated_index[1],inflated_index[2]])
-
-def routine_2d():
-    image = io.imread('/home/tom/Images/autres/berners.jpg', as_gray=True)
-    template=image[270:300,850:900]
-    i_row, i_col = image.shape
-    print(image.shape)
-    t_row, t_col = template.shape
-    t_dep = time.clock()
-    correlation = fft_nxcorr(image, template)
-    maxind = np.unravel_index(np.argmax(correlation, axis=None), correlation.shape)
-    print("correlation construite en ", time.clock() - t_dep)
-    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2, num='ND Template Search')
-    ax1.imshow(image, interpolation='nearest')
-    ax1.set_title('Search image')
-    ax2.imshow(template, interpolation='nearest')
-    ax2.set_title('Template')
-    print(maxind)
-    ax3.imshow(correlation, interpolation='nearest')
-    ax3.set_title('Normalized cross-correlation')
-    rect = patches.Rectangle((maxind[1] - int(t_col / 2), maxind[0] - int(t_row / 2)), t_col, t_row, linewidth=1,
-                             edgecolor='r', facecolor='none')
-    ax3.add_patch(rect)
-    plt.show()
-
-if __name__=='__main__':
-    #routine_3d()
-    #routine_2d()
-    #perfo_test()
-    test_classe(58)
 
